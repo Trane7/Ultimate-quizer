269173778d776aa29f54a51e4e32deb246e55f6a
Dee <dgrowette@gmail.com>
2021-10-26 16:48:24 -0700

    took out console.log

--- assets/highscores.js
+++ assets/highscores.js
@@ -2,6 +2,5 @@ const highScoresList = document.querySelector('#highScoresList')
 const highScores = JSON.parse(localStorage.getItem('highScores')) || []
 
 highScoresList.innerHTML = highScores.map(score => {
-    console.log('fire', score)
     return `<li class="high-score">${score.name} - ${score.score}</li>`
 }).join('')
