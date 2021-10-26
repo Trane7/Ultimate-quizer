2d35b71cd76508901a7ee2b1b6cd4728e27ee09e
Dee <dgrowette@gmail.com>
2021-10-26 15:31:39 -0700

    fixed some highscores bugs

--- assets/end.js
+++ assets/end.js
@@ -29,6 +29,6 @@ saveHighScore = e => {
 
     highScores.splice(5)
 
-    localStorage.setItem('highScore', JSON.stringify(highScores))
-    window.location.assign('/')
+    localStorage.setItem('highScores', JSON.stringify(highScores))
+    window.location.assign('./highScores.html')
 }
